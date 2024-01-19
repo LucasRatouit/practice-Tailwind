c770356d8d75c5632ba5b88db15730c70480b499
Lucas Ratouit <ratouitlucaspro@gmail.com>
2024-01-19 12:16:15 +0100

    feature: add preview images theme

--- src/components/slide.tsx
+++ src/components/slide.tsx
@@ -4,12 +4,14 @@ type SlideProps = {
     position: "left-0" | "right-0"
 };
 
+const maxPage = 4;
+
 const Slide = ({ page, setPage }) => {
     function switchComponent(e) {
         const id = e.currentTarget.id
         if (id === "1" && page > 1) {
             setPage(page-1)
-        } else if (id === "2" && page < 3 ) {
+        } else if (id === "2" && page < maxPage) {
             setPage(page+1)
         }
     }
@@ -22,7 +24,7 @@ const Slide = ({ page, setPage }) => {
         <div>
             <Slide id="1" src="left" position="left-0" />
             <Slide id="2" src="right" position="right-0" />
-            <p className="left-1/2 bottom-6 opacity-80 absolute">{page} / 3</p>
+            <p className="left-1/2 bottom-6 opacity-80 absolute">{page} / {maxPage}</p>
         </div>
     )
 }
